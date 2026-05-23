0ae12cc3e7ce3260a889d9e4a85333a7c3a70351
BUPANA VARSHITH <vbvarshith@gmail.com>
2026-05-23 15:56:40 +0530

    Fixed root page deployment issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,5 @@
-import { redirect } from 'next/navigation';
-
-import { createClient } from '@/lib/supabase/server';
-
 import HomePage from '@/components/HomePage';
 
-export default async function Home() {
-
-  const supabase =
-    await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  /* NOT LOGGED IN */
-
-  if (!user) {
-    redirect('/signup');
-  }
-
-  /* LOGGED IN */
-
+export default function Home() {
   return <HomePage />;
 }
